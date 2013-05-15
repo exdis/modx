d5769413dcc5717bb792fcf6f9c4fddfde520036
exdis <exsdis@gmail.com>
2013-05-15 15:12:31 +0700

    Добавлен elFinder, Траслитерация алиасов и файлов, обновлен дамп

--- manager/media/browser/mcpuk/browser.php
+++ manager/media/browser/mcpuk/browser.php
@@ -1,52 +1,51 @@
-<?php
-$selfpos = strlen('manager/media/browser/mcpuk/browser.php');
-$base_path = substr(str_replace('\\','/',__FILE__), 0, $selfpos * -1);
-define('MODX_BASE_PATH', $base_path);
-$rb = new FBROWSER();
-$ph = array();
-$ph['seturl_js'] = $rb->seturl_js();
-$output = $rb->render_fbrowser($ph);
-header('X-UA-Compatible: IE=EmulateIE7');
-echo $output;
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>elFinder 2.0</title>
 
-class FBROWSER
-{
-	function seturl_js()
-	{
-		$editor = (isset($_GET['editor'])) ? $_GET['editor'] : '';
-		$seturl_js_filename = ($editor!=='' && !stristr($editor,"..")) ? 'seturl_js_'  . htmlspecialchars($editor) . '.inc' : '';
-		$seturl_js_path = MODX_BASE_PATH . 'assets/plugins/';
-		
-		if($seturl_js_filename!='' && file_exists($seturl_js_path . $seturl_js_filename))
-		{
-			$result = file_get_contents($seturl_js_path . $seturl_js_filename);
-		}
-		else
-		{
-			switch($editor)
-			{
-				case 'tinymce' :
-				case 'tinymce3':
-					$editor_path = isset($_GET['editorpath']) ? htmlspecialchars($_GET['editorpath'], ENT_QUOTES) : '';
-					$result = file_get_contents('seturl_js_tinymce.inc');
-					$result = str_replace('[+editor_path+]', $editor_path, $result);
-					break;
-				default:
-				$result = '<script src="seturl.js" type="text/javascript"></script>' . PHP_EOL;
-			}
-		}
-		return $result;
-	}
-	
-	function render_fbrowser($ph)
-	{
-		$browser_html = file_get_contents('browser.html.inc');
-		$browser_html2 = $browser_html;
-		foreach($ph as $name => $value)
-		{
-			$name = '[+' . $name . '+]';
-			$browser_html = str_replace($name, $value, $browser_html);
-		}
-		return $browser_html;
-	}
-}
+		<!-- jQuery and jQuery UI (REQUIRED) -->
+		<link rel="stylesheet" type="text/css" media="screen" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+
+		<!-- elFinder CSS (REQUIRED) -->
+		<link rel="stylesheet" type="text/css" media="screen" href="css/elfinder.min.css">
+		<link rel="stylesheet" type="text/css" media="screen" href="css/theme.css">
+
+		<!-- elFinder JS (REQUIRED) -->
+		<script type="text/javascript" src="js/elfinder.min.js"></script>
+
+		<!-- elFinder translation (OPTIONAL) -->
+		<script type="text/javascript" src="js/i18n/elfinder.ru.js"></script>
+<?php
+if(($_GET['editor'] == 'tinymce3' || $_GET['editor'] == 'tinymce') && $_GET['editorpath'])
+	$editorPath = htmlspecialchars($_GET['editorpath'], ENT_QUOTES);
+?>
+		<script type="text/javascript" src="<?php echo $editorPath; ?>/jscripts/tiny_mce/tiny_mce_popup.js"></script>
+		<script type="text/javascript" src="<?php echo $editorPath; ?>/tinymce.modxfb.js"></script>
+
+		<!-- elFinder initialization (REQUIRED) -->
+		<script type="text/javascript" charset="utf-8">
+			$().ready(function() {
+				$('body').css('font-size','100%'); //font bug
+				var elf = $('#elfinder').elfinder({
+					url : 'php/connector.php?type=<?php echo $_GET['type']; ?>',
+					lang: 'ru',
+					height: $(window).height(),
+					getFileCallback : function(url) {
+						url = url.substr(1); // remove slash
+						FileBrowserDialogue.selectURL(url);
+						window.close();
+                    }
+				}).elfinder('instance');
+			});
+		</script>
+	</head>
+	<body style="margin:0;padding:0;">
+
+		<!-- Element where elFinder will be created (REQUIRED) -->
+		<div id="elfinder"></div>
+
+	</body>
+</html>
